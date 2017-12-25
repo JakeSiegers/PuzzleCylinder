76b7089105a0bd115eb8c4cabae5fa31ac5d58fd
Jake Siegers <JakeSiegers@users.noreply.github.com>
2017-12-24 22:35:21 -0600

    Updated noCache Technique

--- game/PuzzleInit.js
+++ game/PuzzleInit.js
@@ -1,8 +1,4 @@
-//This code checks for a date file in your browsers local storage.
-//That file is always cache-bursted, so it's always up to date.
-//If the date in the file is newer than in your browsers local storage, I know to cache-burst the entire page
-//This makes sure you're running the latest version of the game.
-
+//Get the build date!
 let PG = null;
 let lastUpdateTime = 0;
 
@@ -17,19 +13,7 @@ http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 http.onreadystatechange = function() {//Call a function when the state changes.
 	if(http.readyState == 4 && http.status == 200) {
 		lastUpdateTime = http.responseText;
-		if (typeof(Storage) !== "undefined") {
-			console.log("Your Version: "+localStorage.getItem("version"));
-			console.log("Server Version: "+http.responseText);
-			if(http.responseText !== localStorage.getItem("version")){
-				localStorage.setItem("version", http.responseText);
-				location.reload(true);
-			}else{
-				PG = new PuzzleGame();
-			}
-		}else{
-			PG = new PuzzleGame();
-		}
+		PG = new PuzzleGame();
 	}
 };
-
 http.send(null);
--- updateVersion.js
+++ updateVersion.js
@@ -1,2 +1,7 @@
 let fs = require("fs");
-fs.writeFileSync(process.argv[2]+'/version.html',Math.floor(Date.now()/1000));
+let timestamp = Math.floor(Date.now()/1000);
+fs.writeFileSync(process.argv[2]+'/version.html',timestamp);
+
+let html = fs.readFileSync(process.argv[2]+'/index_template.html','utf8');
+html = html.replace(/{noCache}/g,'?v='+timestamp);
+fs.writeFileSync(process.argv[2]+'/index.html',html);
